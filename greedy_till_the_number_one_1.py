8ded741860738f3b8bd3442a61d22c1c8af20f6f
Jinah-engineer <jeeeengineer@gmail.com>
2023-03-16 00:19:44 +0900

    [2023.03.16] greedy

---
+++ greedy_till_the_number_one_1.py
@@ -0,0 +1,19 @@
+# 내가 푼 문제
+#n, k = map(int, input().split())
+n = 25
+k = 5
+
+count = 0
+count += n % k
+
+if n % k != 0:
+    for i in range(n % k):
+        n -= 1
+
+while True: 
+    n //= k
+    count += 1
+    if n == 1 :
+        break
+
+print(count)
