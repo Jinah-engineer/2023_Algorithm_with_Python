466085925f8cebbffa2fe6148fbb31465187c5c4
Jinah-engineer <jeeeengineer@gmail.com>
2023-03-23 00:14:23 +0900

    [2023.03.23] dfs (1)

--- implementation_time.py
+++ implementation_time.py
@@ -7,18 +7,10 @@ count = 0
     n % 10 = 3
 '''
 
-h = 0
-m = 0
-s = 0
 
-for i in range(n):
-    if i % 10 == 3:
-        h += 1
+for i in range(n + 1):
     for j in range(60):
-        if j % 10 == 3:
-            m += 1
         for k in range(60):
-            if k % 10 == 3:
-                s += 1      
-count = h * m * s
+           if '3' in str(i) + str(j) + str(k):
+               count += 1
 print(count)
