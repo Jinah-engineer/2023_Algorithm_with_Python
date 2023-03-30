fe58a5c91e65ba8547117dfe5d002847710fb200
Jinah Park <jinahisworking@gmail.com>
2023-03-30 17:47:16 +0900

    [2023.03.30]

--- implementation_LRUD.py
+++ implementation_LRUD.py
@@ -1,17 +1,19 @@
 n = int(input())
-x, y = 1, 1
+a, b = 1, 1
 plans = input().split()
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+# (i , j)
+di = [0, 0, -1, 1] # left side
+dj = [-1, 1, 0, 0] # right side
 
 move_types = ['L', 'R', 'U', 'D']
 
 for plan in plans:
     # plan은 이동계획
     for i in range(len(move_types)):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        if plan == move_types[i]:
+            nx = a + di[i]
+            ny = b + dj[i]
     
     if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
